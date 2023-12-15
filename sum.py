f2ecf06ae424aeba92aacbd282da12ce958a86c6
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2023-12-15 15:25:57 -0600

    refactored program to calculate the average

--- sum.py
+++ sum.py
@@ -2,6 +2,6 @@ def mysum(list, num):
     output = 0
     for number in list:
         output += number
-    return output + num
+    return (output + num)/(len(list) + 1)
 
 print(mysum([10, 20, 30, 40], 50))
