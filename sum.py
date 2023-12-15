565ebf9804b79cb57c425e2e5bb2bab21f9f37be
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2023-12-15 15:27:35 -0600

    refactored program to calculate sum of list and num

--- sum.py
+++ sum.py
@@ -2,6 +2,6 @@ def mysum(list, num):
     output = 0
     for number in list:
         output += number
-    return (output + num)/(len(list) + 1)
+    return output + num
 
 print(mysum([10, 20, 30, 40], 50))
